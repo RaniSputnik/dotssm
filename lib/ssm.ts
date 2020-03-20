9d0c6e457aed719cd83c9bb25dd1a2e7f4010c02
Ryan Loader <talk@ryanloader.me>
2020-03-20 21:26:33 +0000

    Read all pages from SSM

--- lib/ssm.ts
+++ lib/ssm.ts
@@ -1,25 +1,38 @@
 import AWS from "aws-sdk";
 import { GetConfigFunc, Config } from "./types";
 
+const PAGE_SIZE = 10; // Currently the maximum page size for loading from SSM
+const MAX_PAGES = 100; // For a maximum limit of 1000 params loaded, should be plenty
+
 export const ssm = (
   client: AWS.SSM = new AWS.SSM()
 ): GetConfigFunc => async namespace => {
-  const params: AWS.SSM.GetParametersByPathRequest = {
-    Path: namespace,
-    Recursive: true,
-    WithDecryption: true
-    // TODO: Handle pagination
-  };
-  const res = await client.getParametersByPath(params).promise();
   const result: Config = {};
 
-  res.Parameters?.forEach(p => {
-    if (!p.Name || !p.Value) {
-      return;
+  let nextToken = undefined;
+  for (var i = 0; i < MAX_PAGES; i++) {
+    const params: AWS.SSM.GetParametersByPathRequest = {
+      Path: namespace,
+      Recursive: true,
+      WithDecryption: true,
+      MaxResults: PAGE_SIZE,
+      NextToken: nextToken
+    };
+    const res = await client.getParametersByPath(params).promise();
+
+    res.Parameters?.forEach(p => {
+      if (!p.Name || !p.Value) {
+        return;
+      }
+      const name = p.Name.substr(namespace.length);
+      result[name] = p.Value;
+    });
+
+    nextToken = res.NextToken;
+    if (!nextToken) {
+      return result;
     }
-    const name = p.Name.substr(namespace.length);
-    result[name] = p.Value;
-  });
+  }
 
   return result;
 };
