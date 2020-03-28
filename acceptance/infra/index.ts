077c585c90395dff449786ebc22af5b25d2aa0dd
Ryan Loader <talk@ryanloader.me>
2020-03-28 22:09:16 +0000

    Remove unused pulumi packages

--- acceptance/infra/index.ts
+++ acceptance/infra/index.ts
@@ -1,6 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
 const params = {
   "/some/namespace/greeting": "Hello, world",
