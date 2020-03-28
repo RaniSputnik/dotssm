40d9cbf903ea5a1f39c8286f4d84489d51b91688
Ryan Loader <talk@ryanloader.me>
2020-03-28 21:59:39 +0000

    Update acceptance test infra to Pulumi

---
+++ acceptance/infra/index.ts
@@ -0,0 +1,34 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+const params = {
+  "/some/namespace/greeting": "Hello, world",
+
+  "/foo/bar": "bar",
+  "/foo/secret": "somethingsecret",
+  "/foo/nested/value": "this config is nested",
+
+  "/pagination/foo": "bar",
+  "/pagination/baz": "barry",
+  "/pagination/hello": "world",
+  "/pagination/cake": "is a lie",
+  "/pagination/something": "someone",
+  "/pagination/someway": "somehow",
+  "/pagination/tip": "top",
+  "/pagination/gate": "way",
+  "/pagination/rani": "sputnik",
+  "/pagination/project": "dotssm",
+  "/pagination/page": "2"
+};
+
+Object.entries(params).map(
+  ([name, value]) =>
+    new aws.ssm.Parameter(name, {
+      name: name,
+      description: "Fixture for dotssm acceptance tests",
+      type: "String",
+      value: value,
+      overwrite: true
+    })
+);
